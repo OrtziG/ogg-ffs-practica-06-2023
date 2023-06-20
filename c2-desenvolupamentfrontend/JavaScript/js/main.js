c0e096454a4c99357629b8a32a666e0a965f5864
OrtziG <ortzi98@gmail.com>
2023-06-20 15:27:16 +0200

    js

---
+++ c2-desenvolupamentfrontend/JavaScript/js/main.js
@@ -0,0 +1,14 @@
+//Ejemplo de uso de JS
+// El script alert muestra un pop up con el texto dentro de los apóstrofes.
+alert('Hello World');
+
+// Las variables en ems5 > es var, y de ems6 < es let. También existen las const, un tipo de variable SIEMPRE en mayús que no se pueden modificar una vez seteadas.
+
+var nombre = 'Ortzi';
+let edad= 3892;
+const IVA = 0.21;
+let ValorA = true;
+let ValorB = null;
+let ValorC = undefined;
+
+
